7a2077594fd09e9dd0f5fe219a4141d2c318529f
Raphael Simon <simon.raphael@gmail.com>
2023-05-22 19:06:23 -0700

    Use testify assert in log tests (#241)

--- log/options_test.go
+++ log/options_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/sdk/trace"
@@ -14,88 +15,63 @@ import (
 
 func TestDefaultOptions(t *testing.T) {
 	opts := defaultOptions()
-	if fmt.Sprintf("%p", opts.disableBuffering) != fmt.Sprintf("%p", IsTracing) {
-		t.Errorf("got disable buffering %p, expected %p", &opts.disableBuffering, IsTracing)
-	}
-	if opts.debug {
-		t.Errorf("expected debug to be disabled")
-	}
-	if opts.w != os.Stdout {
-		t.Errorf("got output %p, expected os.Stdout", opts.w)
-	}
-	if fmt.Sprintf("%p", opts.format) != fmt.Sprintf("%p", FormatText) {
-		t.Errorf("got format %p, expected %p", opts.format, FormatText)
-	}
-	if opts.maxsize != DefaultMaxSize {
-		t.Errorf("got maxsize %d, expected %d", opts.maxsize, DefaultMaxSize)
-	}
+	assert.Equal(t, fmt.Sprintf("%p", opts.disableBuffering), fmt.Sprintf("%p", IsTracing))
+	assert.False(t, opts.debug)
+	assert.Equal(t, opts.w, os.Stdout)
+	assert.Equal(t, fmt.Sprintf("%p", opts.format), fmt.Sprintf("%p", FormatText))
+	assert.Equal(t, opts.maxsize, DefaultMaxSize)
 }
 
 func TestWithDisableBuffering(t *testing.T) {
 	opts := defaultOptions()
 	disable := func(ctx context.Context) bool { return true }
 	WithDisableBuffering(disable)(opts)
-	if fmt.Sprintf("%p", opts.disableBuffering) != fmt.Sprintf("%p", disable) {
-		t.Errorf("got disable buffering %p, expected %p", opts.disableBuffering, disable)
-	}
+	assert.Equal(t, fmt.Sprintf("%p", opts.disableBuffering), fmt.Sprintf("%p", disable))
 }
 
 func TestWithDebug(t *testing.T) {
 	opts := defaultOptions()
 	WithDebug()(opts)
-	if !opts.debug {
-		t.Errorf("expected debug to be enabled")
-	}
+	assert.True(t, opts.debug)
 }
 
 func TestWithNoDebug(t *testing.T) {
 	opts := defaultOptions()
 	WithDebug()(opts)
 	WithNoDebug()(opts)
-	if opts.debug {
-		t.Errorf("expected debug to be disabled")
-	}
+	assert.False(t, opts.debug)
 }
 
 func TestWithOutput(t *testing.T) {
 	opts := defaultOptions()
 	w := io.Discard
 	WithOutput(w)(opts)
-	if fmt.Sprintf("%p", opts.w) != fmt.Sprintf("%p", w) {
-		t.Errorf("got output %p, expected %p", opts.w, w)
-	}
+	assert.Equal(t, opts.w, w)
 }
 
 func TestWithFormat(t *testing.T) {
 	opts := defaultOptions()
 	WithFormat(FormatJSON)(opts)
-	if fmt.Sprintf("%p", opts.format) != fmt.Sprintf("%p", FormatJSON) {
-		t.Errorf("got format %p, expected %p", opts.format, FormatJSON)
-	}
+	assert.Equal(t, fmt.Sprintf("%p", opts.format), fmt.Sprintf("%p", FormatJSON))
 }
 
 func TestWithMaxSize(t *testing.T) {
 	opts := defaultOptions()
 	WithMaxSize(10)(opts)
-	if opts.maxsize != 10 {
-		t.Errorf("got maxsize %d, expected 10", opts.maxsize)
-	}
+	assert.Equal(t, opts.maxsize, 10)
 }
 
 func TestIsTracing(t *testing.T) {
 	if IsTracing(context.Background()) {
 		t.Errorf("expected IsTracing to return false")
 	}
-
 	exp, _ := stdouttrace.New(stdouttrace.WithWriter(io.Discard))
 	tp := trace.NewTracerProvider(trace.WithBatcher(exp))
 	defer tp.Shutdown(context.Background())
 	otel.SetTracerProvider(tp)
 	ctx, span := otel.Tracer("test").Start(context.Background(), "test")
 	defer span.End()
-	if !IsTracing(ctx) {
-		t.Errorf("expected IsTracing to return true")
-	}
+	assert.True(t, IsTracing(ctx))
 }
 
 func TestIsTerminal(t *testing.T) {
